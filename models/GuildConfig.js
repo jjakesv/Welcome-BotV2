ab3809f12c585b59d2749ceda3e53b08773eeeaf
Umbra Dev <prifdgamer23@gmail.com>
2025-10-26 18:36:07 +0530

    Create GuildConfig.js

---
+++ models/GuildConfig.js
@@ -0,0 +1,50 @@
+/**
+ * Guild Configuration Schema
+ * Made by Umbra X Development - https://discord.gg/Whq4T2vYPP
+ */
+
+const mongoose = require('mongoose');
+
+const guildConfigSchema = new mongoose.Schema({
+  guildId: { type: String, required: true, unique: true },
+  
+  // Welcome settings
+  welcomeChannelId: { type: String, default: null },
+  welcomeMessage: { 
+    type: String, 
+    default: 'Welcome {user} to {server}! 🎉\n\n*Bot made by Umbra X Development - https://discord.gg/Whq4T2vYPP*' 
+  },
+  welcomeCardEnabled: { type: Boolean, default: true },
+  welcomeCardBackground: { 
+    type: String, 
+    default: 'https://images.unsplash.com/photo-1579546929518-9e396f3cc809' 
+  },
+  welcomeDmEnabled: { type: Boolean, default: true },
+  welcomeDmMessage: { 
+    type: String, 
+    default: 'Welcome to {server}! Please read the rules and have fun!\n\n*Powered by Umbra X Development*\nhttps://discord.gg/Whq4T2vYPP' 
+  },
+  welcomeEmoji: { type: String, default: '👋' },
+  
+  // Goodbye settings
+  goodbyeChannelId: { type: String, default: null },
+  goodbyeMessage: { 
+    type: String, 
+    default: 'Goodbye {user}! We\'ll miss you 😢\n\n*Bot made by Umbra X Development - https://discord.gg/Whq4T2vYPP*' 
+  },
+  goodbyeCardEnabled: { type: Boolean, default: true },
+  goodbyeCardBackground: { 
+    type: String, 
+    default: 'https://images.unsplash.com/photo-1557683316-973673baf926' 
+  },
+  
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+});
+
+guildConfigSchema.pre('save', function(next) {
+  this.updatedAt = Date.now();
+  next();
+});
+
+module.exports = mongoose.model('GuildConfig', guildConfigSchema);
